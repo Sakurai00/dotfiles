5273cbc601c344e8893817f739f320a0e8b4c397
Sakurai <K.Sakurai_2000@outlook.com>
2020-12-25 15:22:27 +0900

    rm sudo

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt upgrade -y
+apt update
+apt upgrade -y
 
-sudo apt install -y git
+apt install -y git
 
-sudo apt install -y python3
-sudo apt install -y build-essential
-sudo apt install -y gdb
+apt install -y python3
+apt install -y build-essential
+apt install -y gdb
 
 
-sudo apt install -y zsh
+apt install -y zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 
-sudo apt install -y neovim
+apt install -y neovim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 
-sudo apt install -y tmux
+apt install -y tmux
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
 cargo install exa
-sudo apt install -y bat
+apt install -y bat
