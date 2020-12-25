0696e08bf45bf665b15ae3aeebf594717978c7cc
Sakurai <K.Sakurai_2000@outlook.com>
2020-12-25 15:42:09 +0900

    fix :)

--- install.sh
+++ install.sh
@@ -10,17 +10,23 @@ sudo apt install -y build-essential
 sudo apt install -y gdb
 
 
+# zsh
 sudo apt install -y zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 
+# neovim
 sudo apt install -y neovim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 
+
+# tmux
 sudo apt install -y tmux
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+
+# rust & rust tool (exa, bat)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install exa
 sudo apt install -y bat
