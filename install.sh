2a71231f5800a1c2da4822e1fb403849521d8343
Sakurai <54164011+Sakurai00@users.noreply.github.com>
2020-12-26 00:29:02 +0900

    fix #9, #10

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ sudo apt install -y gdb
 
 # zsh
 sudo apt install -y zsh
-chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
 
@@ -28,5 +27,6 @@ sudo apt install -y tmux
 
 # rust & rust tool (exa, bat)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+exec $SHELL -l
 cargo install exa
 sudo apt install -y bat
