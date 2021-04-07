d7cdc40ef0094761c6f44de6a6e35f7d1c03b792
sakurai <K.Sakurai_2000@outlook.com>
2021-04-07 21:43:05 +0900

    add #36

--- script/install.sh
+++ script/install.sh
@@ -3,6 +3,19 @@
 
 sudo apt update
 
+if !(type curl > /dev/null 2>&1); then
+    sudo apt install -y curl
+fi
+
+if !(type wget > /dev/null 2>&1); then
+    sudo apt install -y wget
+fi
+
+# font install
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o "MesloLGS NF Regular.ttf"
+sudo mv "MesloLGS NF Regular.ttf" /usr/share/fonts
+fc-cache -fv
+
 
 ./script/installer/c-cpp.sh
 
