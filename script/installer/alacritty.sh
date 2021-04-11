d4442573275ce476f4a8f1b574c7e57320de588e
sakurai <K.Sakurai_2000@outlook.com>
2021-04-11 13:11:25 +0900

    add #39

--- script/installer/alacritty.sh
+++ script/installer/alacritty.sh
@@ -15,10 +15,10 @@ cargo build --release
 
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
 
-sudo cp target/release/alacritty /usr/local/bin
-sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-sudo desktop-file-install extra/linux/Alacritty.desktop
-sudo update-desktop-database
+#sudo cp target/release/alacritty /usr/local/bin
+#sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+#sudo desktop-file-install extra/linux/Alacritty.desktop
+#sudo update-desktop-database
 
 sudo mkdir -p /usr/local/share/man/man1
 gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
