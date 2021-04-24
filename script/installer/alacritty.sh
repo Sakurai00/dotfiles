d617c4297271629919d045dbeaee07273bdff6af
sakurai <K.Sakurai_2000@outlook.com>
2021-04-24 16:48:41 +0900

    fix #54

--- script/installer/alacritty.sh
+++ script/installer/alacritty.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -xeu
 #rq git, rust
+. ./script/function.sh
 
 if [ ! -d ~/alacritty ]; then
     git clone https://github.com/alacritty/alacritty.git ~/alacritty
@@ -15,10 +16,12 @@ cargo build --release
 
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
 
-#sudo cp target/release/alacritty /usr/local/bin
-#sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-#sudo desktop-file-install extra/linux/Alacritty.desktop
-#sudo update-desktop-database
+if !(is_WSL); then
+    sudo cp target/release/alacritty /usr/local/bin
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+fi
 
 sudo mkdir -p /usr/local/share/man/man1
 gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
