6d6ce77a595ac34b86494a3e23cfc3a5d6ae134e
Sakurai <54164011+Sakurai00@users.noreply.github.com>
2021-05-27 03:32:28 +0900

    Update font.sh

--- script/installer/font.sh
+++ script/installer/font.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -x
 
+sudo apt install -y gnome-font-viewer
+
+if [ ! -d ~/.local/share/fonts ]; then
+    mkdir ~/.local/share/fonts
+fi
+
 curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o "MesloLGS NF Regular.ttf"
-sudo mv "MesloLGS NF Regular.ttf" /usr/share/fonts
+sudo mv "MesloLGS NF Regular.ttf" ~/.local/share/fonts
 fc-cache -fv
