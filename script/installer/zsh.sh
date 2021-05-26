28bdcdc4508170d1c546c58e63127e4eaeb7dcad
Sakurai <54164011+Sakurai00@users.noreply.github.com>
2021-05-27 03:52:22 +0900

    Update zsh.sh

--- script/installer/zsh.sh
+++ script/installer/zsh.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y zsh
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-if [ ! -d ~/.zshrc ]; then
+if [ -d ~/.zshrc ]; then
     rm ~/.zshrc
 fi
 
