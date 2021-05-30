da09a57b8789ae98cf7aba8ec2a9d5b472f8cbd5
sakurai <K.Sakurai_2000@outlook.com>
2021-05-31 02:21:02 +0900

    fix #65

--- script/installer/font.sh
+++ script/installer/font.sh
@@ -9,6 +9,6 @@ if [ ! -d ~/.local/share/fonts ]; then
     mkdir ~/.local/share/fonts
 fi
 
-curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o "MesloLGS NF Regular.ttf"
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o "MesloLGS NF Regular.ttf"
 sudo mv "MesloLGS NF Regular.ttf" ~/.local/share/fonts
 fc-cache -fv
