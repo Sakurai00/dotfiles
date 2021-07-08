1bbe3d32df60f0c898df6bda324ac8c5569e03ad
sakurai <K.Sakurai_2000@outlook.com>
2021-07-08 10:58:56 +0900

    fix #72

--- script/installer/zsh.sh
+++ script/installer/zsh.sh
@@ -4,9 +4,5 @@
 echo_module_name zsh
 
 sudo apt install -y zsh
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-if [ -d ~/.zshrc ]; then
-    rm ~/.zshrc
-fi
 
 #chsh -s $(which zsh)
