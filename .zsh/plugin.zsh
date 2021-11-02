f0dac9bb7b21d8bda2304d3559b24d2a6422ecb7
Sakurai <K.Sakurai_2000@outlook.com>
2021-11-02 11:14:33 +0900

    fix: #95 zinit source

--- .zsh/plugin.zsh
+++ .zsh/plugin.zsh
@@ -2,7 +2,7 @@
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
     print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
     command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.zinit/bin" && \
         print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
         print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
@@ -23,17 +23,17 @@ zinit ice depth=1; zinit light romkatv/powerlevel10k
 
 
 # ====== Load Zinit plugins ======
-# https://zdharma.github.io/zinit/wiki/Example-Minimal-Setup/
+# https://zdharma-continuum.github.io/zinit/wiki/Example-Minimal-Setup/
 zinit wait lucid light-mode for \
   atinit"zicompinit; zicdreplay" \
-    zdharma/fast-syntax-highlighting \
+    zdharma-continuum/fast-syntax-highlighting \
   atload"_zsh_autosuggest_start" \
     zsh-users/zsh-autosuggestions \
   blockf atpull'zinit creinstall -q .' \
     zsh-users/zsh-completions \
 
 zinit wait'1' lucid light-mode for \
-  zdharma/history-search-multi-word
+  zdharma-continuum/history-search-multi-word
 # ====== ====== ======
 
 # ====== Load anyenv======
