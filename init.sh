4277130b84ca9d11cbaebc739aa6b5a766cea385
Sakurai <K.Sakurai_2000@outlook.com>
2021-11-06 11:16:27 +0900

    fix: init

--- init.sh
+++ init.sh
@@ -6,7 +6,12 @@ if ! type git >/dev/null 2>&1; then
     sudo apt install -y git
 fi
 
-git clone https://github.com/Sakurai00/dotfiles.git "$HOME/dotfiles"
+if [ ! -d "$HOME/dotfiles" ]; then
+    git clone https://github.com/Sakurai00/dotfiles.git "$HOME/dotfiles"
+else
+    cd "$HOME/dotfiles" || exit
+    git pull
+fi
 
 source "$HOME/dotfiles/script/set_path.sh"
 
