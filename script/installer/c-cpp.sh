4f8f41a7a6f0f100ba4dfe0256e0b28cfde2c182
Sakurai <K.Sakurai_2000@outlook.com>
2021-11-26 22:07:50 +0900

    feat: #104 add clang lldb

--- script/installer/c-cpp.sh
+++ script/installer/c-cpp.sh
@@ -4,10 +4,10 @@ function install_c-cpp() {
     echo_module_name C/C++
 
     # Install C/C++
-    sudo apt install -y build-essential gdb
+    sudo apt install -y build-essential gdb clang lldb cmake
 }
 
 function update_c-cpp() {
     # Update C/C++
-    sudo apt upgrade -y build-essential gdb
+    sudo apt upgrade -y build-essential gdb clang lldb cmake
 }
