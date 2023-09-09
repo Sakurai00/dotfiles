66f70563e9e9b20b7d41f455ee58cadf7a2fbb82
Sakurai <K.Sakurai_2000@outlook.com>
2023-09-09 17:12:56 +0900

    feat:#137 exa->eza

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,11 +4,11 @@ if type apt-fast > /dev/null 2>&1; then
     alias apt="apt-fast"
 fi
 
-if type exa > /dev/null 2>&1; then
-    alias ls="exa -F"
-    alias lt="exa -aT -I '.git|.github'"
-    alias ll="exa -alF --git"
-    alias llt="exa -alFT -I '.git'"
+if type eza > /dev/null 2>&1; then
+    alias ls="eza -F"
+    alias lt="eza -aT -I '.git|.github'"
+    alias ll="eza -alF --git"
+    alias llt="eza -alFT -I '.git'"
 fi
 
 alias cpi="cp -i"
