ca522fb0ae27b49252978424da9630a87f9e7173
Sakurai <K.Sakurai_2000@outlook.com>
2023-09-10 07:55:18 +0900

    fix: some packages

--- init.sh
+++ init.sh
@@ -14,8 +14,13 @@ else
     git pull
 fi
 
+if ! type pip >/dev/null 2>&1; then
+    sudo apt install -y pip
+fi
+
 if ! type ansible >/dev/null 2>&1; then
     pip install --user ansible
+    export PATH="$HOME/.local/bin:$PATH"
 fi
 
 ansible-playbook ./ansible/playbook.yml --ask-become-pass --tags minimum,normal
