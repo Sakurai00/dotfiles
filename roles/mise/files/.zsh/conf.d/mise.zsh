eb0f46c07c6d8bfd605b381969968193d4fdee3b
Sakurai <K.Sakurai00@outlook.com>
2025-06-29 13:17:52 +0900

    fix: mise load bug

--- roles/mise/files/.zsh/conf.d/mise.zsh
+++ roles/mise/files/.zsh/conf.d/mise.zsh
@@ -1,5 +1,5 @@
 # ====== Load mise ======
-if [ -d "$HOME/.local/bin/mise" ] ; then
-  eval "$($HOME/.local/bin/mise activate zsh)"
+if [ -f "$HOME/.local/bin/mise" ] ; then
+  eval "$(mise activate zsh)"
 fi
 # ====== ====== ======
